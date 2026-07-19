91f6f05e3bacc24809bcf18c938128100858fea4
ajeetbisht1103-creator <ajeetbisht1103@gmail.com>
2026-07-19 17:55:49 +0530

    Add program to display a personalized greeting using input

---
+++ Unit-02-Strings/01_GoodAfternoon.py
@@ -0,0 +1,15 @@
+# ----------------------------------------------------
+# Topic: Input and Output
+#
+# Description:
+# This program accepts the user's name as input
+# and displays a personalized greeting.
+#
+# Question:
+# Write a Python program to display a user entered
+# name followed by "Good Afternoon" using input().
+# ----------------------------------------------------
+
+name = input("Enter your name: ")
+
+print(f"Good Afternoon, {name}!")
