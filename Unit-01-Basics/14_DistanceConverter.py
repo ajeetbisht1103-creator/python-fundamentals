79f482798279a0d1293a1989dc8a3ff40aca713f
ajeetbisht1103-creator <ajeetbisht1103@gmail.com>
2026-07-21 20:53:39 +0530

    Add kilometer to mile converter

---
+++ Unit-01-Basics/14_DistanceConverter.py
@@ -0,0 +1,11 @@
+# ----------------------------------------------------
+# Description:
+# This program converts kilometers into
+# miles.
+# ----------------------------------------------------
+
+kilometers = float(input("Enter distance in kilometers: "))
+
+miles = kilometers * 0.621371
+
+print(f"Distance in Miles: {miles:.2f}")
