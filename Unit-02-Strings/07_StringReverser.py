e59d11f41d2810899bb439bdb80316db135e09a2
ajeetbisht1103-creator <ajeetbisht1103@gmail.com>
2026-07-24 22:59:01 +0530

    Add string reversal program

---
+++ Unit-02-Strings/07_StringReverser.py
@@ -0,0 +1,11 @@
+# ----------------------------------------------------
+# Description:
+# This program reverses the given string
+# using Python slicing.
+# ----------------------------------------------------
+
+text = input("Enter a string: ")
+
+reversed_text = text[::-1]
+
+print(f"Reversed String: {reversed_text}")
