97d062681d7c41086c962d44b431591cd393bc9b
ajeetbisht1103-creator <ajeetbisht1103@gmail.com>
2026-07-25 22:09:54 +0530

    Add title case converter

---
+++ Unit-02-Strings/12_TitleCaseConverter.py
@@ -0,0 +1,12 @@
+# ----------------------------------------------------
+# Description:
+# This program converts the first letter
+# of every word in a sentence to uppercase.
+# ----------------------------------------------------
+
+sentence = input("Enter a sentence: ")
+
+title_case = sentence.title()
+
+print("Title Case:")
+print(title_case)
