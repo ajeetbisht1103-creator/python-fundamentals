37fd0fc5a861591effdb8e641678e3ec3042f19a
ajeetbisht1103-creator <ajeetbisht1103@gmail.com>
2026-08-01 14:07:02 +0530

    Add tuple packing and unpacking program

---
+++ 03_Collections/12_TuplePackingUnpacking.py
@@ -0,0 +1,14 @@
+# ----------------------------------------------------
+# Description:
+# This program demonstrates tuple
+# packing and unpacking.
+# ----------------------------------------------------
+
+student = ("Ajeet", 19, "CSE")
+
+name, age, branch = student
+
+print("Packed Tuple:", student)
+print("Name:", name)
+print("Age:", age)
+print("Branch:", branch)
