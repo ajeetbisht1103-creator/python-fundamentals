4773e7b22fc1d0e20d58ace3017913b7e2fbec27
ajeetbisht1103-creator <ajeetbisht1103@gmail.com>
2026-08-09 10:30:08 +0530

    Add prime number checker using function

---
+++ Unit-04-Functions-and-Recursion/11_PrimeNumberChecker.py
@@ -0,0 +1,23 @@
+# ----------------------------------------------------
+# Description:
+# This program checks whether a number is prime
+# using a user-defined function.
+# ----------------------------------------------------
+
+def is_prime(number):
+    if number < 2:
+        return False
+
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+
+    return True
+
+
+number = int(input("Enter a number: "))
+
+if is_prime(number):
+    print("The number is Prime.")
+else:
+    print("The number is not Prime.")
